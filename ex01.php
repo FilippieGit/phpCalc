45090b46ef6790538703e1e926846429b0bc1c20
FilippieGit <filippieamaral@gmail.com>
2024-09-02 20:21:49 -0300

    v2

--- ex01.php
+++ ex01.php
@@ -2,7 +2,7 @@
 
 echo 'Escolha uma das seguintes:<br><br>1 - Adição<br>2 - Subtração<br>3 - Multiplicação<br>4 - Divisão<br><br><br>Seu cálculo:<br><br>';
 
-$n1=10; // Defina o primeiro valor
+$n1=30; // Defina o primeiro valor
 $n2=50; // Defina o segundo valor
 
 // 1 - Adição
@@ -10,46 +10,46 @@ $n2=50; // Defina o segundo valor
 // 3 - Multiplicação
 // 4 - Divisão
 
-$escolha=1;
-
-function adicao($n1, $n2){
-
-	return($n1+$n2);
-
-}
-
-function subtracao($n1, $n2){
-
-	return($n1-$n2);
-
-}
-
-function multiplicacao($n1, $n2){
-
-	return($n1*$n2);
-
-}
-
-function divisao($n1, $n2){
-
-	return($n1/$n2);
-
-}
+$escolha=2;
 
 switch ($escolha) {
 	case 1: // Adição
+		function adicao($n1, $n2){
+
+			return($n1+$n2);
+
+		}
+
 		echo "$n1 + $n2 = " . adicao($n1, $n2);
 		break;
 
 	case 2: // Subtração
+		function subtracao($n1, $n2){
+
+			return($n1-$n2);
+
+		}
+
 		echo "$n1 - $n2 = " . subtracao($n1, $n2);
 		break;
 
 	case 3: // Multiplicação
+		function multiplicacao($n1, $n2){
+
+			return($n1*$n2);
+
+		}
+
 		echo "$n1 * $n2 = " . multiplicacao($n1, $n2);
 		break;
 
 	case 4: // Divisão
+		function divisao($n1, $n2){
+
+			return($n1/$n2);
+
+		}
+
 		echo "$n1 / $n2 = " . divisao($n1, $n2);
 		break;
 	
@@ -59,32 +59,6 @@ switch ($escolha) {
 		
 		break;
 }
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 
 
 ?>
